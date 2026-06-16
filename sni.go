68c44a311706bff3ea4c5503bc3364ff7873a4fb
Rob van Oostenrijk <robvanoostenrijk@users.noreply.github.com>
2026-06-16 20:51:00 +0400

    Refactor against routines.go

--- sni.go
+++ sni.go
@@ -26,7 +26,7 @@ func (conn readOnlyConn) SetDeadline(t time.Time) error      { return nil }
 func (conn readOnlyConn) SetReadDeadline(t time.Time) error  { return nil }
 func (conn readOnlyConn) SetWriteDeadline(t time.Time) error { return nil }
 
-func peekClientHello(conn net.Conn) (*tls.ClientHelloInfo, io.Reader, error) {
+func peekClientHello(conn net.Conn) (string, io.ReadWriteCloser, error) {
 	peekedBytes := new(bytes.Buffer)
 	var hello *tls.ClientHelloInfo
 
@@ -38,25 +38,39 @@ func peekClientHello(conn net.Conn) (*tls.ClientHelloInfo, io.Reader, error) {
 		},
 	}).Handshake()
 
-	return hello, io.MultiReader(peekedBytes, conn), err
+	wrappedConn := struct {
+		io.Reader
+		io.Writer
+		io.Closer
+	}{
+		Reader: io.MultiReader(peekedBytes, conn),
+		Writer: conn,
+		Closer: conn,
+	}
+
+	var serverName string
+	if hello != nil {
+		serverName = hello.ServerName
+	}
+
+	return serverName, wrappedConn, err
 }
 
 // Get SNI hostname, dial out through tunnel, then proxy data
-func sniProxyForward(dial func(string, string) (net.Conn, error), clientConn net.Conn) error {
-	if err := clientConn.SetReadDeadline(time.Now().Add(5 * time.Second)); err != nil {
+func sniProxyForward(dial func(string, string) (net.Conn, error), conn net.Conn) error {
+	if err := conn.SetReadDeadline(time.Now().Add(5 * time.Second)); err != nil {
 		return fmt.Errorf("set read deadline failed: %w", err)
 	}
 
-	clientHello, peekedClientReader, err := peekClientHello(clientConn)
+	hostname, clientConn, err := peekClientHello(conn)
 	if err != nil {
 		return fmt.Errorf("peek client hello failed: %w", err)
 	}
 
-	if err := clientConn.SetReadDeadline(time.Time{}); err != nil {
+	if err := conn.SetReadDeadline(time.Time{}); err != nil {
 		return fmt.Errorf("clear read deadline failed: %w", err)
 	}
 
-	hostname := clientHello.ServerName
 	if hostname == "" {
 		return fmt.Errorf("no SNI hostname in ClientHello")
 	}
@@ -66,24 +80,16 @@ func sniProxyForward(dial func(string, string) (net.Conn, error), clientConn net
 	if err != nil {
 		return fmt.Errorf("tun tcp dial failed: %w", err)
 	}
-	defer func() { _ = backendConn.Close() }()
 
-	go func() {
-		_, _ = io.Copy(backendConn, peekedClientReader)
-		if tcpConn, ok := backendConn.(interface{ CloseWrite() error }); ok {
-			_ = tcpConn.CloseWrite()
-		}
-	}()
-
-	_, _ = io.Copy(clientConn, backendConn)
+	go connForward(clientConn, backendConn)
+	go connForward(backendConn, clientConn)
 
 	return nil
 }
 
 func sniServe(dial func(string, string) (net.Conn, error), conn net.Conn) {
-	defer func() { _ = conn.Close() }()
-
 	if err := sniProxyForward(dial, conn); err != nil {
 		log.Printf("SNI proxy: %s\n", err)
+		_ = conn.Close()
 	}
 }
